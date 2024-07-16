58b77690698d98ee313939ee8bce0294f9667bdb
pera <nikodijevicp01@gmail.com>
2024-07-16 19:27:20 +0200

    folder strcuture

--- Identity.API/Program.cs
+++ Identity.API/Program.cs
@@ -1,3 +1,17 @@
+using Identity.API.Application;
+using Identity.API.Core.Interfaces;
+using Identity.API.Core.Interfaces.UnitOfWork;
+using Identity.API.Endpoints.Mapster;
+using Identity.API.Infrastructure;
+using Identity.API.Infrastructure.Repositories;
+using Identity.API.Infrastructure.Repositories.UnitOfWork;
+using Mapster;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using System.Text;
 
 namespace Identity.API
 {
@@ -7,16 +21,54 @@ namespace Identity.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                //serverOptions.Limits.MaxConcurrentConnections = 5000;
+                //serverOptions.Limits.MaxConcurrentUpgradedConnections = 5000;
+
+                //ThreadPool.SetMinThreads(200, 200);
+
+                serverOptions.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http1AndHttp2);
+
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<UsersContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddTransient<IUserRepository, UserRepository>();
+            builder.Services.AddTransient<UserService>();
+            builder.Services.AddTransient<AuthenticationService>();
+
+            builder.Services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JWT")["Key"] ?? ""))
+                };
+            });
+
+            builder.Services.AddMapster();
+            MapsterConfig.Configure();
+
+            builder.Services.AddHttpClient();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -27,10 +79,28 @@ namespace Identity.API
 
             app.UseAuthorization();
 
-
+            app.UseMiddleware<ProtocolLoggingMiddleware>();
             app.MapControllers();
 
             app.Run();
+        }
+    }
+    public class ProtocolLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ProtocolLoggingMiddleware> _logger;
+
+        public ProtocolLoggingMiddleware(RequestDelegate next, ILogger<ProtocolLoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogWarning($"Protocol: {context.Request.Protocol}");
+
+            await _next(context);
         }
     }
 }
